30b3689349e9541f66e877fcb7b26dc868d42022
irina <eiv2212@gmail.com>
2022-12-07 17:27:20 +0300

    Задача 1 корректировка

--- Task_1.py
+++ Task_1.py
@@ -6,10 +6,9 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-n = float(input('Введите число: '))
-tot = 0
-while (n > 0):
-    dig = n % 10
-    tot = tot + dig
-    n = n // 10
-print('Сумма цифр =', tot)
+number = input('Введите число: ')
+sum = 0
+for i in number:
+    if i != '.' and i != '-':
+        sum += int(i)
+print(f'Сумма цифр = {sum}')
